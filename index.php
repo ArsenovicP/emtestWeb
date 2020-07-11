a98967bc8e74656801b0afa940722179cbb61879
Jaroslav Beredi <xarocx@gmail.com>
2020-07-11 18:46:16 +0200

    update1

--- index.php
+++ index.php
@@ -1,7 +1,52 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: xaroc
- * Date: 7/11/2020
- * Time: 6:25 PM
- */
+<!doctype html>
+
+
+<html>
+
+    <head>
+        <title>Pohovor</title>
+    </head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+
+    <body>
+    <div class="container">
+
+        <h1 class="text-info">Pohovor</h1>
+
+        <div class="row">
+
+            <div class="col-sm-3">
+                <input type="number" id="count" name="count" class="form-control">
+            </div>
+            <div class="col-sm-2">
+                <button class="btn btn-success" id="button">Generovať</button>
+            </div>
+
+        </div>
+        <div class="row">
+            <div id="result">
+            </div>
+        </div>
+
+    </div>
+        <script>
+            $(document).ready(function(){
+                $("button").click(function(){
+
+
+                    $.post("script.php", {count: $('#count').val()} , function(data){
+
+                        $("#result").html(data);
+                    });
+                });
+            });
+        </script>
+    </body>
+
+</html>
---
+++ script.php
@@ -0,0 +1,11 @@
+<?php
+
+    $data = array();
+
+    for ($i = 0; $i < $_POST['count']; $i++){
+        array_push($data, array('ID' => $i, 'nazov' => $i.'-vehicle', 'rychlost' => rand(5,100)));
+    }
+
+    echo json_encode($data);
+
+?>
